e60f897fc9632d5eba5e1afe478f625d36a179d5
Ger Kersten <werfberg@gmail.com>
2017-07-04 18:08:07 +0200

    Revert "Cosmetic: formatted with astyle to improve readability. No content change."
    This reverts commit dff55e674da7c9f50b644b5093a5d5054c18fd03.

--- network/tcp-server.cc
+++ network/tcp-server.cc
@@ -13,153 +13,153 @@ namespace zhmu {
 namespace snippets {
 
 TCPServer::TCPServer()
-    : m_ServerFD(-1), m_HaveThread(false)
+	: m_ServerFD(-1), m_HaveThread(false)
 {
 }
 
 TCPServer::~TCPServer()
 {
-    Stop();
+	Stop();
 }
 
 bool TCPServer::Start(int port_number)
 {
-    assert(m_ServerFD < 0); // already started?
+	assert(m_ServerFD < 0); // already started?
 
-    m_ServerFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (m_ServerFD < 0)
-        return false;
+	m_ServerFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_ServerFD < 0)
+		return false;
 
-    // Enable address re-use so we don't have to wait until it expires
-    {
-        int on = 1;
-        setsockopt(m_ServerFD, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-    }
+	// Enable address re-use so we don't have to wait until it expires
+	{
+		int on = 1;
+    setsockopt(m_ServerFD, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+  }
 
-    // Bind to our port number
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(port_number);
-    if (bind(m_ServerFD, (struct sockaddr*)&sin, sizeof(sin)) < 0)
-        return false;
+	// Bind to our port number
+	struct sockaddr_in sin;
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port_number);
+	if (bind(m_ServerFD, (struct sockaddr*)&sin, sizeof(sin)) < 0)
+		return false;
 
-    // We want incoming connections
-    if (listen(m_ServerFD, 5) < 0)
-        return false;
+	// We want incoming connections
+	if (listen(m_ServerFD, 5) < 0)
+		return false;
 
-    // And we have a thread to take care of them
-    if (pthread_create(&m_Thread, NULL, &ThreadWrapper, this) < 0)
-        return false;
-
-    m_HaveThread = true;
-    return true;
+	// And we have a thread to take care of them
+	if (pthread_create(&m_Thread, NULL, &ThreadWrapper, this) < 0)
+		return false;
+	
+	m_HaveThread = true;
+	return true;
 }
 
 void TCPServer::Stop()
 {
-    if (m_ServerFD < 0)
-        return; // wasn't started
+	if (m_ServerFD < 0)
+		return; // wasn't started
+ 
+	// Get rid of the server; the select() call will stop
+	close(m_ServerFD);
+	m_ServerFD = -1;
 
-    // Get rid of the server; the select() call will stop
-    close(m_ServerFD);
-    m_ServerFD = -1;
+	// If we have a thread, get rid of it - it will have stopped with the loop as
+	// we have closed the server socket
+	if (m_HaveThread) {
+		pthread_join(m_Thread, NULL);
+		m_HaveThread = false;
+	}
 
-    // If we have a thread, get rid of it - it will have stopped with the loop as
-    // we have closed the server socket
-    if (m_HaveThread) {
-        pthread_join(m_Thread, NULL);
-        m_HaveThread = false;
-    }
-
-    // Get rid of any clients, now that the thread is gone
-    for(TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); it++)
-        delete *it;
-    m_Clients.clear();
+	// Get rid of any clients, now that the thread is gone
+	for(TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); it++)
+		delete *it;
+	m_Clients.clear();
 }
 
 void* TCPServer::ThreadWrapper(void* context)
 {
-    ((TCPServer*)context)->Thread();
-    return NULL;
+	((TCPServer*)context)->Thread();
+	return NULL;
 }
 
 void TCPServer::Thread()
 {
-    while(m_ServerFD >= 0) {
-        // Construct the list of file descriptors; this will be the server and all clients
-        int max_fd = m_ServerFD;
-        fd_set fds;
-        FD_ZERO(&fds);
-        FD_SET(m_ServerFD, &fds);
-        for(TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); it++) {
-            Client& client = **it;
+	while(m_ServerFD >= 0) {
+		// Construct the list of file descriptors; this will be the server and all clients
+		int max_fd = m_ServerFD;
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(m_ServerFD, &fds);
+		for(TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); it++) {
+			Client& client = **it;
 
-            // Add the client's file descriptor and update the max
-            FD_SET(client.GetFD(), &fds);
-            max_fd = std::max(max_fd, client.GetFD());
-        }
+			// Add the client's file descriptor and update the max
+			FD_SET(client.GetFD(), &fds);
+			max_fd = std::max(max_fd, client.GetFD());
+		}
 
-        // Wait until some socket has some event to report
-        int n = select(max_fd + 1, &fds, NULL, NULL, NULL);
-        if (n < 0)
-            break;
+		// Wait until some socket has some event to report
+		int n = select(max_fd + 1, &fds, NULL, NULL, NULL);
+		if (n < 0)
+			break;
 
-        // If our server socket has an event, it's a new client
-        if (FD_ISSET(m_ServerFD, &fds)) {
-            // New connection; accept it
-            int fd = accept(m_ServerFD, NULL, NULL);
-            if (fd >= 0) {
-                Client* client = CreateClient(fd);
-                if (client != NULL) {
-                    // Client accepted; add to the list of clients
-                    m_Clients.push_back(client);
-                } else {
-                    // Client rejected; ditch the connection
-                    close(fd);
-                }
-            }
-        }
+		// If our server socket has an event, it's a new client
+		if (FD_ISSET(m_ServerFD, &fds)) {
+			// New connection; accept it
+			int fd = accept(m_ServerFD, NULL, NULL);
+			if (fd >= 0) {
+				Client* client = CreateClient(fd);
+				if (client != NULL) {
+					// Client accepted; add to the list of clients
+					m_Clients.push_back(client);
+				} else {
+					// Client rejected; ditch the connection
+					close(fd);
+				}
+			}
+		}
 
-        for (TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); /* nothing */) {
-            Client& client = **it;
-            if (!FD_ISSET(client.GetFD(), &fds)) {
-                // Not this client; ignore
-                it++;
-                continue;
-            }
+		for (TClientPtrVector::iterator it = m_Clients.begin(); it != m_Clients.end(); /* nothing */) {
+			Client& client = **it;
+			if (!FD_ISSET(client.GetFD(), &fds)) {
+				// Not this client; ignore
+				it++;
+				continue;
+			}
 
-            if (client.OnEvent()) {
-                // Client accepted the event; continue
-                it++;
-                continue;
-            }
+			if (client.OnEvent()) {
+				// Client accepted the event; continue
+				it++;
+				continue;
+			}
 
-            // Clean up the client
-            it = m_Clients.erase(it);
-            delete &client;
-        }
-    }
+			// Clean up the client
+			it = m_Clients.erase(it);
+			delete &client;
+		}
+	}
 }
 
 TCPServer::Client::Client(int fd)
-    : m_FD(fd)
+	: m_FD(fd)
 {
 }
 
 TCPServer::Client::~Client()
 {
-    close(m_FD);
+	close(m_FD);
 }
 
 int TCPServer::Client::GetFD() const
 {
-    return m_FD;
+	return m_FD;
 }
 
 int TCPServer::Client::Read(void* buf, int length)
 {
-    return (int)recv(m_FD, buf, length, 0);
+	return (int)recv(m_FD, buf, length, 0);
 }
 
 
--- network/tcp-server.h
+++ network/tcp-server.h
@@ -11,78 +11,78 @@ namespace snippets {
 class TCPServer
 {
 public:
-    TCPServer();
-    virtual ~TCPServer();
+	TCPServer();
+	virtual ~TCPServer();
 
-    /*! \brief Creates a TCP server on a given port number
-     *  \param port_number TCP port to bind to
-    *  \returns true on success
-    */
-    bool Start(int port_number);
+	/*! \brief Creates a TCP server on a given port number
+	 *  \param port_number TCP port to bind to
+   *  \returns true on success
+   */
+	bool Start(int port_number);
 
-    //! \brief Closes the TCP server, dropping all connections
-    void Stop();
+	//! \brief Closes the TCP server, dropping all connections
+	void Stop();
 
-    //! \brief Client beloning to this server
-    class Client
-    {
-    public:
-        Client(int fd);
-        virtual ~Client();
+	//! \brief Client beloning to this server
+	class Client
+	{
+	public:
+		Client(int fd);
+		virtual ~Client();
 
-        /*! \brief Called when a socket event has occured
-         *  \returns true to continue, false to close the connection
-         *
-         *  This function should read the socket as well, as this is the event we'll trigger.
-         */
-        virtual bool OnEvent() = 0;
+		/*! \brief Called when a socket event has occured
+		 *  \returns true to continue, false to close the connection
+	 	 *
+		 *  This function should read the socket as well, as this is the event we'll trigger.
+		 */
+		virtual bool OnEvent() = 0;
 
-        /*! \brief Sends data to the client
-         *  \param buf Buffer to send
-         *  \param length Number of bytes to send
-         *  \returns Amount send, or -1 on failure
-         */
-        int Write(const void* buf, int length);
+		/*! \brief Sends data to the client
+		 *  \param buf Buffer to send
+		 *  \param length Number of bytes to send
+		 *  \returns Amount send, or -1 on failure
+		 */
+		int Write(const void* buf, int length);
 
-        /*! \brief Receives data from the client
-         *  \param buf Buffer to fill
-         *  \param length Number of bytes to read
-         *  \returns Amount read, or -1 on failure
-         */
-        int Read(void* buf, int length);
+		/*! \brief Receives data from the client
+		 *  \param buf Buffer to fill
+		 *  \param length Number of bytes to read
+		 *  \returns Amount read, or -1 on failure
+		 */
+		int Read(void* buf, int length);
 
-        //! \brief Retrieves the client's file descriptor in use
-        int GetFD() const;
+		//! \brief Retrieves the client's file descriptor in use
+		int GetFD() const;
 
-    private:
-        int m_FD;
-    };
-    typedef std::vector<Client*> TClientPtrVector;
+	private:
+		int m_FD;
+	};
+	typedef std::vector<Client*> TClientPtrVector;
 
-    /*! \brief Called when a new client is about to be accepted
-     *  \param fd File descriptor of the client
-     *  \returns Client pointer, or NULL if the client is to be dropped
-     */
-    virtual Client* CreateClient(int fd) = 0;
+	/*! \brief Called when a new client is about to be accepted
+	 *  \param fd File descriptor of the client
+	 *  \returns Client pointer, or NULL if the client is to be dropped
+	 */
+	virtual Client* CreateClient(int fd) = 0;
 
 protected:
-    //! \brief Thread code
-    void Thread();
+	//! \brief Thread code
+	void Thread();
 
-    //! \brief Wrapper for pthread
-    static void* ThreadWrapper(void* context);
+	//! \brief Wrapper for pthread
+	static void* ThreadWrapper(void* context);
 
-    //! \brief Server file descriptor, or -1 if none
-    int m_ServerFD;
+	//! \brief Server file descriptor, or -1 if none
+	int m_ServerFD;
 
-    //! \brief Server thread accepting clients, if any
-    pthread_t m_Thread;
+	//! \brief Server thread accepting clients, if any
+	pthread_t m_Thread;
 
-    //! \brief Do we have a server thread?
-    bool m_HaveThread;
+	//! \brief Do we have a server thread?
+	bool m_HaveThread;
 
-    //! \brief Clients
-    TClientPtrVector m_Clients;
+	//! \brief Clients
+	TClientPtrVector m_Clients;
 };
 
 }
